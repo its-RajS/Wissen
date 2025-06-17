6fdb33d6733b3484238373c274da905116eaec44
Raj <rajsharma222547@gmail.com>
2025-06-17 21:39:28 +0530

    small change in origin

--- backend/index.ts
+++ backend/index.ts
@@ -25,7 +25,7 @@ app.use(cookieParser());
 const origin = process.env.ORIGIN;
 app.use(
   cors({
-    origin: origin || "http://localhost:3000",
+    origin: origin,
     credentials: true,
   })
 );
